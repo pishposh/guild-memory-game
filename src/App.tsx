564431f9df18e484ed1997e6cc58759e3ea59cea
Justin Fuller <justindanielfuller@gmail.com>
2024-11-04 17:34:01 +0000

    Start Time + Score

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Game, NewGame } from './game';
 
 const CARD_VALUES = [
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
@@ -12,15 +13,40 @@ interface Card {
 
 function App() {
   const [cards, setCards] = useState<Card[]>([...CARD_VALUES, ...CARD_VALUES].map((value, id) => ({ value, id })));
+  const [game, setGame] = useState<Game>(NewGame());
+  const [duration, setDuration] = useState("0m 0s");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDuration(game.getDuration())
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div id='game'>
-      {cards.map(card => (
-        <div className='card' key={card.id}>
-            {card.value}
+    <>
+      <div id='game'>
+        {cards.map(card => (
+          <div className='card' key={card.id}>
+              {card.value}
+          </div>
+        ))}
+      </div>
+      <div className="scoreboard">
+        <p className="time">
+          <strong>Time spent:</strong> {duration}
+        </p>
+        <div className="row">
+          <p className="attempts">
+            <strong>Cards flipped:</strong> {game.getAttempts()}
+          </p>
+          <p className="score">
+            <strong>Matches:</strong> {game.getScore()}
+          </p>
         </div>
-      ))}
-    </div>
+      </div>
+    </>
   )
 }
 
