e3af31e22aafeb650e7d9e444db79a35e589717f
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-11-05 12:23:27 -0500

    show animation when match found

--- src/components/PicketSign/index.tsx
+++ src/components/PicketSign/index.tsx
@@ -3,28 +3,22 @@ import { useEffect, useMemo, useState } from 'react';
 import { Card } from '../../types';
 import './PicketSign.css';
 
-export const PicketSign = ({
-  card
-}: {
-  card: Card
+export const PicketSign = ({ card }: { card: Card }) => {
+  const { value, isFaceUp, isMatched } = card;
 
-}) => {
-  const {value, isFaceUp, isMatched} = card
-
-  const [matched, setMatched] = useState(false)
+  const [matched, setMatched] = useState(false);
 
   useEffect(() => {
     if (isMatched) {
-      setTimeout(() => setMatched(true), 1000)
+      setTimeout(() => setMatched(true), 500);
     }
-  }, [isMatched])
+  }, [isMatched]);
 
   const imageSrc = useMemo(() => `assets/sign-content/${value}`, [value]);
 
   return (
     <div className={clsx('container', isFaceUp ? 'face-up' : 'face-down')}>
-      
-      {!matched &&( 
+      {!matched && (
         <div className="back-container">
           <img
             className="sign-image"
@@ -33,21 +27,21 @@ export const PicketSign = ({
           />
         </div>
       )}
-        <div className={clsx('front-container', matched && 'matched')}>
+      <div className={clsx('front-container', matched && 'matched')}>
+        <img
+          className="sign-image"
+          src="assets/picket-sign/picket-sign-face-up.png"
+          onDragStart={(e) => e.preventDefault()}
+        />
+        <div className="content-container">
           <img
-            className="sign-image"
-            src="assets/picket-sign/picket-sign-face-up.png"
+            className={clsx('content')}
+            src={imageSrc}
+            alt={value}
             onDragStart={(e) => e.preventDefault()}
           />
-          <div className="content-container">
-            <img
-              className={clsx('content')}
-              src={imageSrc}
-              alt={value}
-              onDragStart={(e) => e.preventDefault()}
-            />
-          </div>
-        </div> 
-    </div> 
+        </div>
+      </div>
+    </div>
   );
 };
