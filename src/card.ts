81563f17980d7ec37fe5397bf0772e993b70f9e7
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-11-06 11:00:04 -0500

    add count to card

--- src/card.ts
+++ src/card.ts
@@ -1,33 +1,37 @@
 enum SignContent {
-    ContractSpellingBee = 'contract-spelling-bee.webp',
-    GuildRound = 'guild-logo-round.webp',
-    GuildSquare = 'guild-logo-square.webp',
-    JustCauseWordle = 'just-cause-wordle.webp',
-    MakerWeekGuild = 'maker-week-guild.webp',
-    UnionMadeGithub = 'union-made-github.webp',
-    WuerkerCartoon = 'wuerker-cartoon.webp',
-    ContractCrossword = 'contract-crossword.webp'
+  ContractSpellingBee = 'contract-spelling-bee.webp',
+  GuildRound = 'guild-logo-round.webp',
+  GuildSquare = 'guild-logo-square.webp',
+  JustCauseWordle = 'just-cause-wordle.webp',
+  MakerWeekGuild = 'maker-week-guild.webp',
+  UnionMadeGithub = 'union-made-github.webp',
+  WuerkerCartoon = 'wuerker-cartoon.webp',
+  ContractCrossword = 'contract-crossword.webp'
 }
 
 export interface Card {
-    id: string;
-    value: SignContent;
-    isFaceUp: boolean;
-    isMatched: boolean;
+  id: string;
+  value: SignContent;
+  isFaceUp: boolean;
+  isMatched: boolean;
+  count: number;
 }
 
 const CARD_VALUES = Object.values(SignContent);
 
 export function getInitialCards(): Card[] {
-    const date = new Date().toISOString();
+  const date = new Date().toISOString();
 
-    return [...CARD_VALUES, ...CARD_VALUES]
-        .map((value, index) => ({
-            value,
-            id: date + index,
-            isFaceUp: false,
-            isMatched: false
-        }))
-        // -1, 0, or 1: sorts randomly
-        .sort(() => Math.floor(Math.random() * 3) - 1);
+  return (
+    [...CARD_VALUES, ...CARD_VALUES]
+      .map((value, index) => ({
+        value,
+        id: date + index,
+        isFaceUp: false,
+        isMatched: false,
+        count: 0
+      }))
+      // -1, 0, or 1: sorts randomly
+      .sort(() => Math.floor(Math.random() * 3) - 1)
+  );
 }
